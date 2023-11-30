09d24e0ef64d75816f8635dcbe52096e7bd04121
Sophia Baranova <sonya.brnv@gmail.com>
2023-11-30 21:28:01 +0300

    16 lab was added

---
+++ 16/16.3/16.3/16.3.cpp
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <cstdlib> //для srand(), rand()
+#include <time.h> //для time()
+#define n 3
+#define m 4
+
+int main()
+{
+	int a[n][m], b[m][n], bt[n][m], c[n][m]; //оголошення масивів
+	printf("%12s\n", "matrix a"); //повідомлення про виведення матриці а
+	srand(time(NULL));
+	//цикл для всіх елементів матриці а
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			a[i][j] = rand() % 26; //генерація випадкового числа від 0 до 25
+			printf("%3d ", a[i][j]); //виведення елемента
+		}
+		printf("\n");
+	}
+	printf("matrix b (enter integers from the range [-99;99])\n"); //запрошення ввести елементи матриці b
+	//цикл для всіх елементів матриці b
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("b[%d][%d] = ",i,j); //введення елемента
+			scanf_s("%d", &b[i][j]); //зчитування
+		}
+	}
+	printf("%12s\n", "matrix bt"); //повідомлення про виведення матриці bt
+	//цикл для всіх елементів матриці bt
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			bt[i][j] = b[j][i]; //присвоєння елементу матриці с значення елемента матриці b із взаємнозаміненими індексами
+			printf("%3d ", bt[i][j]); //виведення елемента
+		}
+		printf("\n");
+	}
+	printf("matrix c = a + bt\n"); //повідомлення про виведення матриці с
+	//цикл для всіх елементів матриці с
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			c[i][j] = a[i][j] + bt[i][j]; //обчислення значення елемента
+			printf("%3d ", c[i][j]); //виведення елемента
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ 16/16.4/16.4/16.4.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#define n 6
+#define m 4
+
+void snake(int a, int b) //функція виведення змійки
+{
+	for (int i = 0; i < a - 1; i++) {
+		for (int j = 0; j < b; j++) {
+			printf(" -");
+		}
+		if (i % 2 == 0) {
+			printf("\n");
+			for (int k = 0; k < b * 2; k++) printf(" ");
+			printf("|\n");
+		}
+		else {
+			printf("\n|\n");
+		}
+	}
+	if (a % 2 == 0) {
+		printf("<");
+		for (int j = 0; j < b; j++) printf("- ");
+	}
+	else {
+		for (int j = 0; j < b; j++) printf(" -");
+		printf(">");
+	}
+	printf("\n");
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ukr");
+	int a[n][m]; //оголошення масиву
+	printf("заповнення матрицi %d x %d змiйкою\n", n, m); //повідомлення про заповнення матриці змійкою
+	//виведення "змійки" для більшої наочності
+	snake(n, m);
+	//цикл для всіх елементів масиву
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 0) { //якщо парний рядок (починаючи з 0-го)
+			for (int j = 0; j < m; j++) { //введення елементів зліва направо
+				printf("елемент [%d][%d]: ", i, j);
+				scanf_s("%d", &a[i][j]);
+			}
+		}
+		else { //якщо непарний рядок
+			for (int j = m - 1; j >= 0; j--) { //ввведення елементів справа наліво
+				printf("елемент [%d][%d]: ", i, j);
+				scanf_s("%d", &a[i][j]);
+			}
+		}
+	}
+	printf("%16s\n", "РЕЗУЛЬТАТ"); //повідомлення про виведення результату
+	//цикл для всіх елементів масиву
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%5d ", a[i][j]); //виведення елемента
+		}
+		printf("\n");
+	}
+	return 0;
+}
